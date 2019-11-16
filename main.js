6e83d36a6e0147f24af9aa1ee5b1d5dad8252aeb
yuyao <980903093@qq.com>
2019-11-16 20:13:34 +0800

    .

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
-let n 
+let n
 初始化();
 setInterval(() => {
-  console.log(n)
-  $(`.images > img:nth-child(${x(n)})`).removeClass('current').addClass('leave').one('transitionend', (e) => {
-    $(e.currentTarget).removeClass('leave').addClass('enter')
+  makeLeave(getImage(n)).one('transitionend', (e) => {
+    makeEnter($(e.currentTarget))
   })
-  $(`.images > img:nth-child(${x(n+1)})`).removeClass('enter').addClass('current')
+  makeCurrent(getImage(n + 1))
   n += 1
 }, 2000)
 
-function x(n) {
+function getImage(n) {
+  return $(`.images > img:nth-child(${x(n)})`)
+}
+
+function m(n) {
   if (n > 3) {
     n = n % 3
     if (n === 0) {
@@ -19,52 +22,31 @@ function x(n) {
   return n
 }
 
-function 初始化(){
+function 初始化() {
   n = 1
   $(`.images > img:nth-child(${n})`).addClass('current')
-  .siblings().addClass('enter')
+    .siblings().addClass('enter')
 }
 
-function makeCurrent($node){
+function makeCurrent($node) {
   $node.removeClass('enter leave').addClass('current')
+  return $node
+}
+
+function makeLeave($node) {
+  $node.removeClass('current enter').addClass('leave')
+  return $node
+}
+
+function makeEnter($node) {
+  $node.removeClass('current leave').addClass('enter')
+  return $node
 }
 
 
 
 
 
-/*
-setTimeout(() => {
-  $('.images > img:nth-child(1)').removeClass('current').addClass('leave')
-    .one('transitionend', (e)=>{
-      $(e.currentTarget).removeClass('leave').addClass('enter')
-    })
-  $('.images > img:nth-child(2)').removeClass('enter').addClass('current')
-}, 3000);
-
-setTimeout(() => {
-  $('.images > img:nth-child(2)').removeClass('current').addClass('leave')
-    .one('transitionend', (e)=>{
-      $(e.currentTarget).removeClass('leave').addClass('enter')
-    })
-  $('.images > img:nth-child(3)').removeClass('enter').addClass('current')
-}, 6000);
-
-setTimeout(() => {
-  $('.images > img:nth-child(3)').removeClass('current').addClass('leave')
-    .one('transitionend', (e)=>{
-      $(e.currentTarget).removeClass('leave').addClass('enter')
-    })
-  $('.images > img:nth-child(1)').removeClass('enter').addClass('current')
-}, 9000);
-
-setTimeout(() => {
-  $('.images > img:nth-child(1)').removeClass('current').addClass('leave')
-    .one('transitionend', (e)=>{
-      $(e.currentTarget).removeClass('leave').addClass('enter')
-    })
-  $('.images > img:nth-child(2)').removeClass('enter').addClass('current')
-}, 12000);
 
 
 
@@ -88,68 +70,3 @@ setTimeout(() => {
 
 
 
-
-
-
-
-
-
-
-setTimeout(function(){
-  $('.images>img:nth-child(1)').css({
-    transform: 'translateX(-100%)'
-  })
-
-  $('.images>img:nth-child(2)').css({
-    transform: 'translateX(-100%)'
-  })
-
-  $('.images>img:nth-child(1)').one('transitionend', function(e){
-    $(e.currentTarget).addClass('right').css({transform: 'none'})
-  })
-
-},2000)
-
-setTimeout(function(){
-  $('.images>img:nth-child(2)').css({
-    transform: 'translateX(-200%)'
-  })
-  
-  $('.images>img:nth-child(3)').css({
-    transform: 'translateX(-100%)'
-  })
-
-  $('.images>img:nth-child(2)').one('transitionend', function(e){
-    $(e.currentTarget).addClass('right').css({transform: 'none'})
-  })
-},4000)
-
-setTimeout(function(){
-  $('.images>img:nth-child(3)').css({
-    transform: 'translateX(-200%)'
-  })
-  
-  $('.images>img:nth-child(1)').css({
-    transform: 'translateX(-100%)'
-  })
-
-  $('.images>img:nth-child(3)').one('transitionend', function(e){
-    $(e.currentTarget).addClass('right').css({transform: 'none'})
-  })
-},6000)
-
-setTimeout(function(){
-  $('.images>img:nth-child(1)').css({
-    transform: 'translateX(-200%)'
-  })
-
-  $('.images>img:nth-child(2)').css({
-    transform: 'translateX(-100%)'
-  })
-
-  $('.images>img:nth-child(1)').one('transitionend', function(e){
-    $(e.currentTarget).addClass('right').css({transform: 'none'})
-  })
-
-},8000)
-*/
